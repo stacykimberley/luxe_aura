3acb1d5327cbcd8eb07c3a3155505c3ff763e3ff
HazelHawadi <hawadihazel@icloud.com>
2025-04-22 10:08:26 +0100

    Add home app and templates

--- luxe_aura/settings.py
+++ luxe_aura/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,10 @@ ROOT_URLCONF = 'luxe_aura.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
